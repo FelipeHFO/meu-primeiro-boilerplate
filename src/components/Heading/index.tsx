504437350199d57c142e9699030b5bea5908a1ec
Felipe Oliveira <felipe.henrique.f.o@gmail.com>
2021-02-28 11:23:03 -0300

    Configurando webpack para transpilar códigos TypeScript

---
+++ src/components/Heading/index.tsx
@@ -0,0 +1,8 @@
+import { title } from 'process'
+import React from 'react'
+
+const Heading = ({title = 'Hello World'}) => {
+  return <h1>{title}</h1>
+}
+
+export default Heading
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,20 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable no-undef */
 const path = require('path');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src', 'index.js'),
+  entry: path.resolve(__dirname, 'src', 'index.tsx'),
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js'
   },
-  resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx']
-  },
   devServer: {
     contentBase: path.resolve(__dirname, 'public')
+  },
+  resolve: {
+    plugins: [new TsconfigPathsPlugin()],
+    extensions: ['.js', '.jsx', '.ts', '.tsx']
   },
   module: {
     rules: [
@@ -24,17 +28,10 @@ module.exports = {
       {
         test: /\.css$/,
         exclude: /node_modules/,
-        use: [
-          {
-            loader: 'style-loader'
-          },
-          {
-            loader: 'css-loader'
-          }
-        ]
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }]
       },
       {
-        test: /\.(png|jpe?g|gif|svg)$/,
+        test: /\.(png|gif|jpe?g|svg)$/i,
         exclude: /node_modules/,
         use: {
           loader: 'file-loader'
